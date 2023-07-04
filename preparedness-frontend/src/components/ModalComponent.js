b32815931c343180fd3deb142886a1001ac06bf7
bShinzato <bshinzato@outlook.com>
2023-07-04 11:54:16 -0700

    finished

--- preparedness-frontend/src/components/ModalComponent.js
+++ preparedness-frontend/src/components/ModalComponent.js
@@ -1,7 +1,63 @@
-import React from "react"
+import React, { useState } from "react"
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader, Label, Input } from "reactstrap"
+import { LoremIpsum } from "react-lorem-ipsum"
 
 const ModalComponent = () => {
-  return <></>
+  const [modal, setModal] = useState(false)
+  
+  const [inputText, setInputText]= useState("")
+  
+  const toggle = () => setModal(!modal)
+
+  const handleChange = (e) => {
+   return setInputText(e.target.value)
+}
+
+const handleReset = () => {
+  return setInputText("")
+}
+
+  return (
+    <>
+      <div>
+        <Label for="name">Enter your name</Label>
+          <Input 
+          type="text"
+          placeholder="Enter Name Here"
+          onChange={handleChange} 
+          value={inputText}
+          />
+          <Button onClick={toggle}>
+            Click Me
+          </Button>
+
+          <Button onClick={handleReset}>
+            Reset
+          </Button>
+
+          <Modal isOpen={modal} toggle={toggle}>
+
+          <ModalHeader toggle={toggle}>
+            Hello there {inputText}!
+          </ModalHeader>
+
+          <ModalBody>
+              <LoremIpsum />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button onClick={toggle}>
+              Do Something
+            </Button>{' '}
+
+            <Button onClick={toggle}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+  </>
+  )
 }
 
 export default ModalComponent
